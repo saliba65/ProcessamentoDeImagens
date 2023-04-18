a61808ba03a64bfc906bad516f0a60363c0fe9b1
Ygor <ygor.melo@dtidigital.com.br>
2023-04-18 00:36:14 -0300

    Criação da interface inicial

--- tp.py
+++ tp.py
@@ -1,1 +1,20 @@
-print("Hello world!")
+'''
+    Disciplina: Processamento e Análise de Imagens - Ciência da Computação, PUC Minas
+    Professor: Alexei 
+    Autores: Fernanda Passos, Lucas Saliba e Ygor Melo
+'''
+
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget
+
+# Declarando um objeto para criar a aplicação 
+app = QApplication(sys.argv)
+
+# Declarando um objeto para criação da janela principal
+windown = QWidget()
+windown.resize(800, 600)
+windown.setWindowTitle("Segmentação e Classificação de Imagens Mamográficas - Processamento e Análise de Imagens, PUC Minas")
+windown.show()
+
+app.exec()
+
