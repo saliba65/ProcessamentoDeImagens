409b11640b90b9e4c7b48135981027327da323fc
Lucas Saliba <lucassaliba10@gmail.com>
2023-06-02 16:43:48 -0300

    ✨ Implementing contrast bar

--- tp.py
+++ tp.py
@@ -4,19 +4,46 @@
     Autores: Lucas Saliba (650625) e Ygor Melo
 '''
 
-import tkinter 
+import tkinter
+import cv2
+import numpy as np
 from tkinter.filedialog import askopenfilename
 from PIL import ImageTk, Image 
 
 # Usando tkinter para leitura de imagens
 #Permitindo buscar arquivos png e tiff na biblioteca 
 root = tkinter.Tk() 
+root.geometry("512x512")
 filetypes = (('Png files', '*.png'),('Tiff files', '*.tiff'))
 filename = askopenfilename(filetypes=filetypes)
+# Redimencionando a imagem para o tamanho ideal da janela
+img = Image.open(filename)
+img = img.resize((384,384))
 
-img = ImageTk.PhotoImage(Image.open(filename)) 
+img = ImageTk.PhotoImage(img) 
 panel = tkinter.Label(root, image = img) 
 panel.pack(side = "bottom", fill = "both", 
            expand = "yes") 
+
+#Criacao do slider para alteracao de contraste
+def slider_changed(event):  
+    imgCv2 = cv2.imread(filename)
+    out = cv2.addWeighted(imgCv2, slider.get(), imgCv2, 0, 0)
+    out = cv2.resize(out, (384,384))
+    img = ImageTk.PhotoImage(Image.fromarray(out)) 
+    panel.configure(image=img)
+    panel.image = img
+
+# Valores do slider - 1 a 256
+slider = tkinter.Scale(
+    root,
+    from_=1,
+    to=256,
+    orient='horizontal',
+    command=slider_changed
+) 
+
+slider.pack()
+
 
 root.mainloop()
