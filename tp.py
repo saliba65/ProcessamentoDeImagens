1b60d4bf8722d7ecb9c4071d377af44a10afa8bc
Lucas Saliba <lucassaliba10@gmail.com>
2023-06-06 19:16:10 -0300

    ♻️ Refactor and organize code

--- tp.py
+++ tp.py
@@ -4,62 +4,81 @@
     Autores: Lucas Saliba (650625) e Ygor Melo
 '''
 
-import tkinter
 import cv2
-import numpy as np
-from tkinter.filedialog import askopenfilename
-from PIL import ImageTk, Image 
+import PIL
+import PIL.ImageTk
+import tkinter
+import tkinter.filedialog
+
+# Funcao contraste da imagem
+def scale_event_1(event: str) -> None:
+    image_cv2 = cv2.imread(file_name)
+
+    image_cv2 = cv2.addWeighted(image_cv2, scale_1.get(), 0, 0, 0)
+
+    image_cv2 = cv2.resize(image_cv2, (384, 384))
+    
+    image = PIL.ImageTk.PhotoImage(PIL.Image.fromarray(image_cv2))
+
+    label_1.configure(image=image)
+
+    label_1.image = image
+
+# Funcao segmentacao da imagem
+def scale_event_2(event: str) -> None:
+    image_cv2 = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
+
+    # Aplicando limiarizacao binaria
+    image_cv2 = cv2.threshold(image_cv2, scale_2.get(), 255, cv2.THRESH_BINARY)[1]
+
+    image_cv2 = cv2.resize(image_cv2, (384, 384))
+    
+    image = PIL.ImageTk.PhotoImage(PIL.Image.fromarray(image_cv2))
+
+    label_1.configure(image=image)
+
+    label_1.image = image
 
 # Usando tkinter para leitura de imagens
 #Permitindo buscar arquivos png e tiff na biblioteca 
-root = tkinter.Tk() 
-root.geometry("512x512")
-filetypes = (('Png files', '*.png'),('Tiff files', '*.tiff'))
-filename = askopenfilename(filetypes=filetypes)
-# Redimencionando a imagem para o tamanho ideal da janela
-img = Image.open(filename)
-img = img.resize((384,384))
 
-img = ImageTk.PhotoImage(img) 
-panel = tkinter.Label(root, image = img) 
-panel.pack(side = "bottom", fill = "both", 
-           expand = "yes") 
+root = tkinter.Tk()
+root.geometry('512x512')
+filetypes = (('PNG File', '*.png'), ('TIFF File', '*.tiff'))
+file_name = tkinter.filedialog.askopenfilename(filetypes=filetypes)
+
+# Redimencionando a imagem para o tamanho ideal da janela
+image = PIL.Image.open(file_name)
+image = image.resize((384, 384))
+
+image = PIL.ImageTk.PhotoImage(image)
 
 #Criacao do slider para alteracao de contraste
-def slider_changed(event):  
-    imgCv2 = cv2.imread(filename)
-    out = cv2.addWeighted(imgCv2, slider.get(), imgCv2, 0, 0)
-    out = cv2.resize(out, (384,384))
-    img = ImageTk.PhotoImage(Image.fromarray(out)) 
-    panel.configure(image=img)
-    panel.image = img
-
-# Valores do slider - 1 a 256
-slider = tkinter.Scale(
+scale_1 = tkinter.Scale(
     root,
-    from_=1,
-    to=256,
+    command=scale_event_1,
     orient='horizontal',
-    command=slider_changed
-) 
+    label='CONTRASTE',
+    from_=0,
+    to=255
+)
 
-slider.pack()
+# Criacao do slider de segmentacao
+scale_2 = tkinter.Scale(
+    root,
+    command=scale_event_2,
+    orient='horizontal',
+    label='SEGMENTAÇÃO',
+    from_=0,
+    to=255
+)
 
-# Segmentacao de imagem
-def segmentar_imagem():
-    imgCv2 = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
-    # Aplicando limiarizacao binaria
-    # # Aplicando filtro de Gaus para melhor limiarizacao
-    # out = cv2.GaussianBlur(imgCv2, (7,7), 0)
-    # Valor de decisao = 7
-    out = cv2.threshold(imgCv2, 7, 255, cv2.THRESH_BINARY)[1]
-    out = cv2.resize(out, (384,384))
-    img = ImageTk.PhotoImage(Image.fromarray(out)) 
-    panel.configure(image=img)
-    panel.image = img
+#Chamada de componentes
+label_1 = tkinter.Label(root, image=image)
 
-#Botoes
-button = tkinter.Button(root, text="Segmentar", command=segmentar_imagem)
-button.pack()
+scale_1.pack()
+scale_2.pack()
+
+label_1.pack()
 
 root.mainloop()
