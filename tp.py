abb6238cc55e9aa643ca4d2b5e6f97fffe09b906
Lucas Saliba <lucassaliba10@gmail.com>
2023-06-10 14:37:40 -0300

    ✨ Rede neural convolucional

--- tp.py
+++ tp.py
@@ -10,6 +10,7 @@ import PIL.ImageTk
 import tkinter
 import tkinter.filedialog
 import os
+import time
 import tqdm
 import numpy as np 
 import tensorflow as tf
@@ -73,6 +74,7 @@ def start_program() -> None:
     y_train_binary = convert_binary(y_train)
     y_test_binary = convert_binary(y_test)
 
+    '''
     # Transformacao para numpy array 
     X_train = np.array(X_train, dtype="float32")
     X_train = np.expand_dims(X_train, -1)
@@ -87,9 +89,13 @@ def start_program() -> None:
     #y_test = tf.keras.utils.to_categorical(y_test, 4)
     y_train_binary = np.array(y_train_binary, dtype="float32")
     y_test_binary = np.array(y_test_binary, dtype="float32")
+    '''
 
     # Rede Neural
-    conv_next(X_train, X_test, y_train, y_test)
+    # conv_next(X_train, X_test, y_train, y_test)
+
+    conv(X_train, X_test, y_train_binary, y_test_binary, 2)
+    conv(X_train, X_test, y_train, y_test, 4)
 
 # Verificar se a imagem pertence ao conjunto de testes
 def is_test(file: str) -> bool:
@@ -133,14 +139,6 @@ def read_data() -> tuple:
                         else:
                             X_train.append(image)
                             y_train.append(y_dict[folder[0]])
-
-    # # Conversao para numpy array
-    # # x = matrizes de pixels
-    # X_train = np.array(X_train, dtype=object)
-    # X_test = np.array(X_test, dtype=object)
-    # # y = birads respectivos 
-    # y_train = np.array(y_train, dtype=object)
-    # y_test = np.array(y_test, dtype=object)
 
     return (X_train, X_test, y_train, y_test)
 
@@ -191,6 +189,7 @@ def convert_binary(y_array: list) -> list:
 
     return y_transformed
 
+'''
 # Aplicacao da rede neural ConvNext 
 def conv_next(X_train: np.array, X_test: np.array, y_train: np.array, y_test: np.array) -> None:
     model = tf.keras.applications.ConvNeXtBase(
@@ -214,12 +213,99 @@ def conv_next(X_train: np.array, X_test: np.array, y_train: np.array, y_test: np
     model.fit(X_train, y_train, verbose=1)
 
     print('fit')
+'''
+
+# Rede neural convolucional 
+def conv(X_train: list, X_test: list, y_train: list, y_test: list, classes: int) -> None:
+    start = time.time()
+    
+    # Conversao para numpy array
+    X_train = np.array(X_train, dtype=object)
+    X_test = np.array(X_test, dtype=object)
+    y_train = np.array(y_train, dtype=object)
+    y_test = np.array(y_test, dtype=object)
+
+    # Normalizacao dos dados ( entre 0 e 1 )
+    X_train = X_train.astype('float32') / 255
+    X_test = X_test.astype('float32') / 255
+
+    # Adicionar uma dimensao aos np arrays para atender especificidades da rede
+    X_train = np.expand_dims(X_train, -1)
+    X_test = np.expand_dims(X_test, -1)
+
+    # Criacao de classes para o keras
+    y_train = tf.keras.utils.to_categorical(y_train, classes)
+    y_test = tf.keras.utils.to_categorical(y_test, classes)
+    
+    # Modelo de rede neural convolucional 
+    model = tf.keras.Sequential(
+        [
+            tf.keras.Input(shape=(224, 224, 1)),
+            tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dropout(0.5),
+            tf.keras.layers.Dense(classes, activation="softmax")
+        ]
+    )
+
+    # Compilar o modelo 
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=[
+        "accuracy",
+        tf.keras.metrics.Precision(),
+        tf.keras.metrics.Recall(),
+        tf.keras.metrics.SpecificityAtSensitivity(0.5)
+    ])
+
+    # Realizar ajuste dos dados
+    model.fit(X_train, y_train, batch_size=16, epochs=16, validation_split=0.1)
+
+    # Captacao de informacoes para exibir na tela
+    score = model.evaluate(X_test, y_test, verbose=0)
+
+    f1_score = 2 * (score[2] * score[3]) / (score[2] + score[3])
+
+    # # Modelo Binario
+    # if classes == 2:
+    #     text = f'''
+    #         Binário (2 Classes):\n
+    #         Loss: {score[0]:.4f}
+    #          Acurácia: {score[1]:.4f}
+    #          F1 Score: {f1_score:.4f}
+    #          Precisao: {score[2]:.4f}
+    #          Sensibilidade: {score[3]:.4f}
+    #          Especificidade: {score[4]:.4f}
+    #          Tempo: {time.time() - start:.2f}s
+    #     '''
+        
+    #     label_2.configure(text=text)
+
+    #     label_2.text = text
+        
+    # # Modelo 4 classes
+    # else:
+    #     text = f'''
+    #         4 Classes:\n
+    #         Loss: {score[0]:.4f}
+    #          Acurácia: {score[1]:.4f}
+    #          F1 Score: {f1_score:.4f}
+    #          Precisao: {score[2]:.4f}
+    #          Sensibilidade: {score[3]:.4f}
+    #          Especificidade: {score[4]:.4f}
+    #          Tempo: {time.time() - start:.2f}s
+    #     '''
+        
+    #     label_3.configure(text=text)
+
+    #     label_3.text = text
 
 
 # Usando tkinter para leitura de imagens
 # Permitindo buscar arquivos png e tiff na biblioteca 
 root = tkinter.Tk()
-root.geometry('512x512')
+root.geometry('1024x512')
 filetypes = (('PNG File', '*.png'), ('TIFF File', '*.tiff'))
 file_name = tkinter.filedialog.askopenfilename(filetypes=filetypes)
 
@@ -260,16 +346,21 @@ scale_3 = tkinter.Scale(
 )
 
 #Botoes
-button = tkinter.Button(root, text="Start", command=start_program)
+button = tkinter.Button(root, text="Treinar Modelo", command=start_program)
 
 #Chamada de componentes
 label_1 = tkinter.Label(root, image=image)
+label_2 = tkinter.Label(root, text='')
+label_3 = tkinter.Label(root, text='')
 
 scale_1.pack()
 scale_2.pack()
 scale_3.pack()
 button.pack()
 
+
+label_2.pack()
+label_3.pack()
 label_1.pack()
 
 root.mainloop()
